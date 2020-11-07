2c2dd8d71a7832542e71a022f45a9111f745cfed
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-07 22:39:20 +0800

    copy the origial empty project

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div data-testid="app" className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
