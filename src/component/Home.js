600a702384cefd0f5204f47a08b56b99779736d9
lyxHanHan <yixuan.li1@thoughtworks.com>
2020-11-08 00:12:55 +0800

    add Home page

---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className='list'>
+                <h2>学员列表</h2>
+            </div>
+
+        );
+    }
+}
+export default Home;
